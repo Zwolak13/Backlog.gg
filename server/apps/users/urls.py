0dbdad727e1922705ff118c34384957e14d3b02b
Mikołaj Sosiński <164782094+mik0lajek@users.noreply.github.com>
2026-03-10 18:18:43 +0100

    fixed backend template

--- server/apps/users/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import register, login_view, logout_view, me
-
-urlpatterns = [
-    path("register/", register),
-    path("login/", login_view),
-    path("logout/", logout_view),
-    path("me/", me),
-]
