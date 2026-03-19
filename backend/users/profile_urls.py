c4e7dde855dfde350978437b74f7e18a1e3b0278
Mikołaj Sosiński <164782094+mik0lajek@users.noreply.github.com>
2026-03-19 16:06:15 +0100

    add: endpoints for getting and updating user profile information

--- backend/users/profile_urls.py
+++ backend/users/profile_urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views.profile_views import delete_account, change_password
+from .views.profile_views import delete_account, change_password, get_profile, update_profile
 
 urlpatterns = [
     path("delete/", delete_account),
     path("change-password/", change_password),
+    path("me/", get_profile),
+    path("update/", update_profile),
 ]
